a60e6e1c9d024305ef5112aea756b0c81709047e
SahanaJSJ <sjayaramu@eplus.com>
2019-05-09 21:49:50 +0530

    Added Quazi packages

--- u16img/init.sh
+++ u16img/init.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-set -e
-set -o pipefail
-
-ls -al
-uname -a
+sudo apt update
+sudo apt install bash-completion
+sudo apt-get install -y python-opencv
+sudo apt install -y nginx
+sudo apt-get install -y unicorn
+sudo apt-get install -y curl gnupg build-essential
+sudo apt-get install -y python-pip python-dev
+sudo apt-get install -y software-properties-common
