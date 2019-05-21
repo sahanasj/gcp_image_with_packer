42de84fa74c2172ec27d934c5d137bf26e7c0429
SahanaJSJ <sjayaramu@eplus.com>
2019-05-21 18:34:50 +0530

    Added packer commands

--- quazi/quazi-install.sh
+++ quazi/quazi-install.sh
@@ -7,6 +7,9 @@ apt-get install -y python-pip python-dev
 apt-get install -y software-properties-common
 apt-get install -y default-jdk
 apt-get install -y wget
+wget https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_linux_amd64.zip
+unzip packer_1.4.0_linux_amd64.zip
+sudo mv packer /usr/bin/
 
 # Ruby and Rails Packages
 apt-add-repository -y ppa:rael-gc/rvm
